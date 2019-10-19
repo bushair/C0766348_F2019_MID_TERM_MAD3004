e8c783a2f9e43a68fb917fabbc2bde918ed39cd7
bushair <bushairkm007@gmail.com>
2019-10-19 18:30:43 -0400

    class customer created

---
+++ C0766348_F2019_MID_TERM_MAD3004/Costomer.swift
@@ -0,0 +1,15 @@
+//
+//  Costomer.swift
+//  C0766348_F2019_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-10-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+class Customer : IDisplay
+{
+    
+    
+   
+}
