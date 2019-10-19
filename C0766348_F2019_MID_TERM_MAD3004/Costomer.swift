70bf93db5799fc78fe41013170035b7586791a39
bushair <bushairkm007@gmail.com>
2019-10-19 18:31:37 -0400

    customer variables

--- C0766348_F2019_MID_TERM_MAD3004/Costomer.swift
+++ C0766348_F2019_MID_TERM_MAD3004/Costomer.swift
@@ -10,6 +10,10 @@ import Foundation
 class Customer : IDisplay
 {
     
+    var customerId : String
+    var firstName : String
+    var lastName : String!
+    var fullName : String
+    var email : String
     
-   
 }
