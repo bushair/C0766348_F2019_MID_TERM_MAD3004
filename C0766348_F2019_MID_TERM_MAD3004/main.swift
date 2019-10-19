1fde7e7d1fcfbbc9326530badc43c8a6ec861379
bushair <bushairkm007@gmail.com>
2019-10-19 19:56:16 -0400

    inter bill

--- C0766348_F2019_MID_TERM_MAD3004/main.swift
+++ C0766348_F2019_MID_TERM_MAD3004/main.swift
@@ -14,5 +14,7 @@ var result = date.dateformatter()
 var custDictionary = Dictionary<String,Customer>()
 var totalBill : Float
 
+var inter = Internet(providerName: "telus", gbUsed: 35.8, billId: "I001", billDate: result, billAmount: 84.7, billType: Bill.Types.Internet)
+
 
 
